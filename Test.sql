cde4ac187cbc0b3a5250a0b6a7018a2f79736c8c
gjsanchezm95 <80078945+gjsanchezm95@users.noreply.github.com>
2025-07-17 10:16:31 -0500

    Update Test.sql

--- Test.sql
+++ Test.sql
@@ -1,23 +1,23 @@
 DELETE T
 FROM GHRD2019_Compensation_Hierarchy T
 JOIN #duplicados D ON
-    T.Date_loaded = D.Date_loaded AND
-    T.[internal_id] = D.[internal_id] AND
-    T.[userid] = D.[userid] AND
-    T.[IPI_Code] = D.[IPI_Code] AND
-    T.[Manager_id] = D.[Manager_id] AND
-    T.Comp_Planner_Mgr_Internal_id = D.Comp_Planner_Mgr_Internal_id AND
-    T.Comp_Planner_Mgr_userid = D.Comp_Planner_Mgr_userid AND
-    T.[Override_Comp_Planer] = D.[Override_Comp_Planer] AND
-    T.[Override_Comments] = D.[Override_Comments] AND
-    T.[Bussines_Validation] = D.[Bussines_Validation] AND
-    T.[First_executive_Internal_id] = D.[First_executive_Internal_id] AND
-    T.[First_executive_userid] = D.[First_executive_userid] AND
-    T.[Senior_Leader1_internal_id] = D.[Senior_Leader1_internal_id] AND
-    T.[Senior_Leader1_userid] = D.[Senior_Leader1_userid] AND
-    T.[Senior_Leader2_internal_id] = D.[Senior_Leader2_internal_id] AND
-    T.[Senior_Leader2_userid] = D.[Senior_Leader2_userid] AND
-    T.[Senior_Leader3_internal_id] = D.[Senior_Leader3_internal_id] AND
-    T.[Senior_Leader3_userid] = D.[Senior_Leader3_userid] AND
-    T.current_record = D.current_record
+    ISNULL(T.Date_loaded, '') = ISNULL(D.Date_loaded, '') AND
+    ISNULL(T.[internal_id], '') = ISNULL(D.[internal_id], '') AND
+    ISNULL(T.[userid], '') = ISNULL(D.[userid], '') AND
+    ISNULL(T.[IPI_Code], '') = ISNULL(D.[IPI_Code], '') AND
+    ISNULL(T.[Manager_id], '') = ISNULL(D.[Manager_id], '') AND
+    ISNULL(T.Comp_Planner_Mgr_Internal_id, '') = ISNULL(D.Comp_Planner_Mgr_Internal_id, '') AND
+    ISNULL(T.Comp_Planner_Mgr_userid, '') = ISNULL(D.Comp_Planner_Mgr_userid, '') AND
+    ISNULL(T.[Override_Comp_Planer], '') = ISNULL(D.[Override_Comp_Planer], '') AND
+    ISNULL(T.[Override_Comments], '') = ISNULL(D.[Override_Comments], '') AND
+    ISNULL(T.[Bussines_Validation], '') = ISNULL(D.[Bussines_Validation], '') AND
+    ISNULL(T.[First_executive_Internal_id], '') = ISNULL(D.[First_executive_Internal_id], '') AND
+    ISNULL(T.[First_executive_userid], '') = ISNULL(D.[First_executive_userid], '') AND
+    ISNULL(T.[Senior_Leader1_internal_id], '') = ISNULL(D.[Senior_Leader1_internal_id], '') AND
+    ISNULL(T.[Senior_Leader1_userid], '') = ISNULL(D.[Senior_Leader1_userid], '') AND
+    ISNULL(T.[Senior_Leader2_internal_id], '') = ISNULL(D.[Senior_Leader2_internal_id], '') AND
+    ISNULL(T.[Senior_Leader2_userid], '') = ISNULL(D.[Senior_Leader2_userid], '') AND
+    ISNULL(T.[Senior_Leader3_internal_id], '') = ISNULL(D.[Senior_Leader3_internal_id], '') AND
+    ISNULL(T.[Senior_Leader3_userid], '') = ISNULL(D.[Senior_Leader3_userid], '') AND
+    ISNULL(T.current_record, '') = ISNULL(D.current_record, '')
 WHERE D.RN > 1;
